0e7a3633a3b0c78452169b3f981b0bf8c38703a5
unknown <lordmoncho@gmail.com>
2026-03-03 17:20:09 +0100

    commit

--- src/main/java/es/um/atica/umufly/parking/domain/model/ReservaParking.java
+++ src/main/java/es/um/atica/umufly/parking/domain/model/ReservaParking.java
@@ -3,6 +3,8 @@ package es.um.atica.umufly.parking.domain.model;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import es.um.atica.umufly.vuelos.domain.model.DocumentoIdentidad;
+
 public class ReservaParking {
 
 	private final UUID id;
@@ -21,7 +23,7 @@ public class ReservaParking {
 		ACTIVA, CANCELADA
 	}
 
-	public ReservaParking( UUID id, DocumentoIdentidad identificadorPasajero, TipoReserva tipo, Periodo periodoEstacionamiento, Importe importeInicial, boolean tieneReservaVuelo ) {
+	private ReservaParking( UUID id, DocumentoIdentidad identificadorPasajero, TipoReserva tipo, Periodo periodoEstacionamiento, Importe importeInicial, boolean tieneReservaVuelo ) {
 		this.id = id;
 		this.identificadorPasajero = identificadorPasajero;
 		this.tipo = tipo;
@@ -31,6 +33,29 @@ public class ReservaParking {
 
 		// Regla de negocio: Descuento por reserva de vuelo
 		this.importe = calcularImporteInicial( importeInicial, tieneReservaVuelo );
+	}
+
+	public static ReservaParking of( UUID id, DocumentoIdentidad identificadorPasajero, TipoReserva tipo, Periodo periodoEstacionamiento, Importe importeInicial, Boolean tieneReservaVuelo ) {
+		if ( id == null ) {
+			throw new IllegalArgumentException( "El id de la reserva no puede ser nulo" );
+		}
+		if ( identificadorPasajero == null ) {
+			throw new IllegalArgumentException( "El titular de la reserva no puede ser nulo" );
+		}
+		if ( tipo == null ) {
+			throw new IllegalArgumentException( "El tipo de reserva no puede estar nulo" );
+		}
+		if ( periodoEstacionamiento == null ) {
+			throw new IllegalArgumentException( "El periodo de estacionamiento no puede ser nulo" );
+		}
+		if ( importeInicial == null ) {
+			throw new IllegalArgumentException( "El importe inicial no puede ser nulo" );
+		}
+		if ( tieneReservaVuelo == null ) {
+			throw new IllegalArgumentException( "tieneReservaVuelo no puede ser nulo" );
+		}
+
+		return new ReservaParking( id, identificadorPasajero, tipo, periodoEstacionamiento, importeInicial, tieneReservaVuelo );
 	}
 
 	private Importe calcularImporteInicial( Importe base, boolean tieneReservaVuelo ) {
